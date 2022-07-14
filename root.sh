61c1a456940004afe03497a8e8642e5658ae2e29
aoneone5 <aoneone5@protonmail.com>
2022-07-14 21:50:27 +0530

    Update root.sh

--- root.sh
+++ root.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+sudo pacman -Syu wget 
+sudo dnf install wget
+
+echo "Install"
+
 wget https://github.com/aoneone5/dotfiles/raw/main/install
-curl --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/aoneone5/dotfiles/main/install-vim-plug.sh | sh
+wget https://raw.githubusercontent.com/aoneone5/dotfiles/main/install-vim-plug.sh
 
 chmod +x install
 chmod +x install-vim-plug.sh
